e8bb0fca1cd97d486d5c2a6a7e69d869221cddc9
Meraj Alam <124230839+meraj987@users.noreply.github.com>
2023-10-11 19:59:50 -0400

    Feature: 5-Round RPS Game
    Implement a 5-round Rock, Paper, Scissors game with user prompts, choices, and scorekeeping.

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ function getComputerChoice() {
         return "Paper"
     else
         return "Scissors"
-    
+
   }
  
   function playRound(playerSelection, computerSelection){
@@ -28,3 +28,32 @@ function getComputerChoice() {
 
 console.log(playRound("Rock", getComputerChoice()));
 
+function game (){
+    let playerCount= 0;
+    let computerCount = 0;
+   function playRoundCalculate() {
+    let playerChoice = prompt("What do you choose?")
+    playerChoice = playerChoice.toLowerCase()
+    let computerChoice = getComputerChoice()
+    let result = playRound(playerChoice, computerChoice)
+    console.log(result);
+    if(result.includes("You won boi"))
+    playerCount++
+  else if (result.includes("Lost to a Computer, you noob"))
+    computerCount++
+    console.log(`Player: ${playerCount} | Computer: ${computerCount}`);
+   }
+    playRoundCalculate();
+    playRoundCalculate();
+    playRoundCalculate();
+    playRoundCalculate();
+    playRoundCalculate();
+
+    if (playerCount > computerCount) 
+        console.log("The winner is the player!");
+    else if (computerCount > playerCount)
+        console.log("The winner is the computer!");
+    else 
+      console.log ("Congratulations you both suck equally")
+}
+game()
