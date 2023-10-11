4cd513ace7bcc6e13c0adfa051994b45694922b3
Meraj Alam <124230839+meraj987@users.noreply.github.com>
2023-10-11 11:53:54 -0400

    add game.js

---
+++ game.js
@@ -0,0 +1,30 @@
+function getComputerChoice() {
+    let randomOutput = Math.floor(Math.random() * 3) + 1;
+    if(randomOutput==1)
+        return "Rock"
+    else if (randomOutput==2)
+        return "Paper"
+    else
+        return "Scissors"
+// Return the randomOutput value
+  }
+ 
+  function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase()
+
+       if ( playerSelection == "rock" && computerSelection == "Paper")
+       {
+        return " Lost to a Computer, you noob"
+       }
+       else if ( playerSelection == "rock" && computerSelection == "Scissors")
+       {
+       return " You won boi"
+       }
+    else 
+    {
+        return " Is is a draw"
+    }
+    }
+
+console.log(playRound("Rock", getComputerChoice()));
+
